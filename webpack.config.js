027db10903968a218368aea8dcfabbfa05fc518a
commenthol <commenthol@gmail.com>
2022-04-09 22:36:08 +0200

    chore: bump dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -3,74 +3,35 @@
  */
 
 const path = require('path')
-// const webpack = require('webpack')
-const { createVariants } = require('parallel-webpack')
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const rimraf = require('rimraf').sync
-
-const doAnalyze = process.env.npm_lifecycle_event === 'webpack:analyze'
-
-const isProd = true
-
-const variants = doAnalyze
-  ? {
-      target: [
-        ['commonjs']
-      ]
-    }
-  : {
-      minified: isProd ? [true] : [false],
-      target: [
-        ['umd', 'markedpp']
-      ]
-    }
 
 rimraf('./dist')
 
-function createConfig (options) {
-  const plugins = [
-  ]
-  if (doAnalyze) {
-    plugins.push(new BundleAnalyzerPlugin())
-  }
-
-  return {
-    mode: isProd ? 'production' : 'development',
-    devtool: 'sourcemap',
-    entry: {
-      markedpp: [
-        // '@babel/polyfill',
-        './src/browser.js'
-      ]
-    },
+module.exports = [
+  {
+    mode: 'production',
+    devtool: 'source-map',
+    entry: { markedpp: ['./src/browser.js'] },
     output: {
       path: path.resolve(__dirname, 'dist'),
-      filename: options.target[1] +
-                (options.minified ? '.min' : '') +
-                '.js',
+      filename: 'markedpp.min.js',
       library: 'markedpp',
-      libraryTarget: options.target[0]
+      libraryTarget: 'umd' // 'commonjs'
     },
-    resolve: {
-      // mainFields: ['browser', 'main', 'module']
-    },
-    optimization: {
-      minimize: options.minified
-    },
+    resolve: {},
+    optimization: { minimize: true },
     module: {
-      rules: [{
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: { presets: ['@babel/preset-env'] }
           }
         }
-      }]
+      ]
     },
-    plugins
+    plugins: []
   }
-}
-
-module.exports = createVariants(variants, createConfig)
+]
